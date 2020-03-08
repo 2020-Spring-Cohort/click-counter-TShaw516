823070e87e751ce72bb22940f6f390b94f3cf799
TShaw516 <TShaw516@gmail.com>
2020-03-08 17:09:19 -0400

    Added clicking class

--- src/js/Click.js
+++ src/js/Click.js
@@ -1,1 +1,30 @@
-const updateClicksElement = (testClicksElement, clicksObject) =>
+class ClickCounter {
+    constructor(){
+        this._clicks=0;
+
+    }
+
+    addClick(){
+        this._clicks+=1;
+
+    }
+    addClickCompanion(){
+        this._clicks-=100;
+
+    }
+
+    addClickCompounder(){
+        this._clicks-=10
+
+    }
+
+    getClickTotal(){
+
+    }
+
+    getClicks(){
+        return this._clicks;
+
+    }
+
+}
